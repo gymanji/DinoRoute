071b81625adbac90c21ad9d6e7187c32ac0388fd
Zach Reed <tzreedjr@gmail.com>
2015-01-02 14:07:31 -0500

    minor cors related updates

--- app.js
+++ app.js
@@ -11,12 +11,21 @@ var users = require('./routes/users');
 
 var app = express();
 
+// CORS implementation
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- public/javascripts/MapsController.js
+++ public/javascripts/MapsController.js
@@ -5,15 +5,27 @@ app.controller('MapsController', ['$scope', '$http', function($scope, $http) {
 
 	base_url = "https://maps.googleapis.com/maps/api/directions/json?origin=Atlanta,GA&destination=Marietta,GA&key=AIzaSyDDKB4EBWfJj0i2qb-Qul3qxfv9D7Mk_D0";
 
-	var req = {
-            method: 'GET',
-            url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Atlanta,GA&destination=Marietta,GA&key=AIzaSyDDKB4EBWfJj0i2qb-Qul3qxfv9D7Mk_D0',
-            headers: {
-               'Content-Type': undefined
-               },
-             data: { test: 'test' }
-	 }
+	// var req = {
+ //            method: 'GET',
+ //            url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Atlanta,GA&destination=Marietta,GA&key=AIzaSyDDKB4EBWfJj0i2qb-Qul3qxfv9D7Mk_D0',
+ //            headers: {
+ //               'Content-Type': undefined
+ //               },
+ //             data: { test: 'test' }
+	//  }
 
+	 // CORS request
+	// $http.jsonp(base_url + "&callback=JSON_CALLBACK").success(function(data, status) {
+	// 	$scope.status = status;
+	// 	$scope.data = data
+	// 	console.log(data);
+	// }).error(function(data, status) {
+	// 	$scope.data = data || "Request failed";
+	// 	$scope.status = status;
+	// });
+	// }
+
+	// GET request
 	$http.get(base_url).success(function(data, status) {
 		$scope.status = status;
 		$scope.data = data
@@ -24,10 +36,6 @@ app.controller('MapsController', ['$scope', '$http', function($scope, $http) {
 	});
 	}
 
-
 }]);
 
-app.config(['$httpProvider', function($httpProvider){
-	$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-	}
-]);
+
