9e04db7e908d8a9a5393528d5df9b56b72feaa4b
Zach Reed <tzreedjr@gmail.com>
2015-01-03 13:29:56 -0500

    mas cors attempts

--- app.js
+++ app.js
@@ -5,23 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-// CORS implementation
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// app.use(cors());
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- public/javascripts/MapsController.js
+++ public/javascripts/MapsController.js
@@ -4,38 +4,20 @@ app.controller('MapsController', ['$scope', '$http', function($scope, $http) {
 	$scope.getMapDirections = function () {
 
 	base_url = "https://maps.googleapis.com/maps/api/directions/json?origin=Atlanta,GA&destination=Marietta,GA&key=AIzaSyDDKB4EBWfJj0i2qb-Qul3qxfv9D7Mk_D0";
+	base_url2 = "https://maps.googleapis.com/maps/api/directions/json?origin=Atlanta,GA&destination=Marietta,GA&key=AIzaSyDDKB4EBWfJj0i2qb-Qul3qxfv9D7Mk_D0?callback=JSON_CALLBACK";
+	method = 'GET'
+	method2 = 'JSONP'
 
-	// var req = {
- //            method: 'GET',
- //            url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Atlanta,GA&destination=Marietta,GA&key=AIzaSyDDKB4EBWfJj0i2qb-Qul3qxfv9D7Mk_D0',
- //            headers: {
- //               'Content-Type': undefined
- //               },
- //             data: { test: 'test' }
-	//  }
-
-	 // CORS request
-	// $http.jsonp(base_url + "&callback=JSON_CALLBACK").success(function(data, status) {
-	// 	$scope.status = status;
-	// 	$scope.data = data
-	// 	console.log(data);
-	// }).error(function(data, status) {
-	// 	$scope.data = data || "Request failed";
-	// 	$scope.status = status;
-	// });
-	// }
-
-	// GET request
-	$http.get(base_url).success(function(data, status) {
-		$scope.status = status;
-		$scope.data = data
-		console.log(data);
-	}).error(function(data, status) {
-		$scope.data = data || "Request failed";
-		$scope.status = status;
-	});
+	$http({method: method, url: base_url}).
+		success(function(data, status) {
+			$scope.status = status;
+			$scope.data = data;
+		}).
+		error(function(data, status) {
+			$scope.data = data || "Request failed";
+			$scope.status = status;
+		});
 	}
-
 }]);
 
 
