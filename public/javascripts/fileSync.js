ad94e7749793685b710490218e86b95c60f4748e
Zach Reed <tzreedjr@gmail.com>
2015-01-11 21:35:57 -0500

    file update

---
+++ public/javascripts/fileSync.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Zach on 1/4/15.
+ */
+
+var fs = require('fs');
+var path = "//Users/Zach/Development/GitHub Repos/DinoRoute/DinoRoute/text.log";
+var options = {encoding: 'utf8', flag: 'r'};
+
+fs.readFile(path, options, function(err, data) {
+   if (err) {
+       return console.log(err);
+   }
+    var parsed = JSON.parse(data);
+    console.log(parsed.Message);
+    console.log(parsed.Key);
+});
